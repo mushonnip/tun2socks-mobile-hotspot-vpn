92e936e01150c39a1b8579ac5e9e262deeefff39
mushonnip <mushonnipabu@gmail.com>
2022-06-16 13:57:15 +0700

    add win bin

---
+++ bin/win/Drivers/addtap.bat
@@ -0,0 +1,2 @@
+rem Add a new TAP virtual ethernet adapter
+"tapinstall.exe" install "OemVista.inf" tap0901
---
+++ bin/win/Drivers/deltapall.bat
@@ -0,0 +1,2 @@
+echo WARNING: this script will delete ALL TAP virtual adapters (use the device manager to delete adapters one at a time)
+"tapinstall.exe" remove tap0901
